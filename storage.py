451fe7e3c2c610b3fa4855d147686382ebbd3c51
toni7891 <toniv7891@gmail.com>
2026-03-31 16:31:41 +0300

    modified storage resolution

---
+++ storage.py
@@ -0,0 +1,18 @@
+import json
+
+
+def save_clients(clients, filename="data.json"):
+    """
+        saves client data to data.json database
+    """
+
+    # converts the class type data to dict using the function inside the class (json accepts dict) 
+    # than adds to list with all accounts created for further addition to json file
+    client_data = []
+    for client in clients:
+        data.append(client.to_dict())
+
+    # opens the data.json and writes the new data
+    with open(filename, "w") as file:
+        json.dump(client_data, file, indent=4)
+
