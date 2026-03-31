854513a4b4081eb0e85a8dcbf3cb2aeafd3db3c2
HarelValfish <harelvalfish@gmail.com>
2026-03-31 16:29:44 +0300

    Implement login screen UI with input fields and buttons

---
+++ ui.py
@@ -0,0 +1,109 @@
+import customtkinter as ctk
+
+class LoginScreen(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # setup
+        self.title("Argument capital Login")
+        self.geometry("400x700")
+        self.configure(fg_color="#0A0E27")  # Background
+
+        # Main frame
+        self.main_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.main_frame.pack(expand=True, fill="both", padx=40)
+
+        # * logo
+        # TODO REPLACE "A" LOGO
+        self.logo_label = ctk.CTkLabel(
+            self.main_frame, 
+            text="A", 
+            font=("Inter", 60, "bold"), 
+            text_color="#3B82F6"
+        )
+        self.logo_label.pack(pady=(60, 0))
+        
+        self.bank_name = ctk.CTkLabel(
+            self.main_frame, 
+            text="ARGUMENT\nCAPITAL", 
+            font=("Inter", 24, "bold"), 
+            text_color="white"
+        )
+        self.bank_name.pack(pady=(0, 40))
+
+        # * input fields
+        self.username_entry = ctk.CTkEntry(
+            self.main_frame,
+            placeholder_text="Account ID",
+            height=55,
+            fg_color="#161C30",
+            border_color="#90D5FF",
+            text_color="white",
+            corner_radius=12
+        )
+        self.username_entry.pack(fill="x", pady=10)
+
+        self.password_entry = ctk.CTkEntry(
+            self.main_frame,
+            placeholder_text="PIN",
+            show="*",
+            height=55,
+            fg_color="#161C30",
+            border_color="#90d5ff",
+            text_color="white",
+            corner_radius=12
+        )
+        self.password_entry.pack(fill="x", pady=10)
+
+        # password reset button
+        # TODO connect pin forgot logic
+        self.forgot_btn = ctk.CTkButton(
+            self.main_frame,
+            text="Forgot PIN?",
+            fg_color="transparent",
+            hover=False,
+            text_color="#555E70",
+            font=("Inter", 12),
+            width=10
+        )
+        self.forgot_btn.pack(anchor="e", pady=(0, 20))
+
+        # login button
+        # TODO login button logic transfering to dashboard
+        self.login_btn = ctk.CTkButton(
+            self.main_frame,
+            text="Log In",
+            height=55,
+            fg_color="#3B82F6",
+            hover_color="#2563EB",
+            font=("Inter", 16, "bold"),
+            corner_radius=12
+        )
+        self.login_btn.pack(fill="x", pady=10)
+
+        # # OR text seperating login or admin login
+        self.or_label = ctk.CTkLabel(
+            self.main_frame, 
+            text="────────  OR  ────────", 
+            text_color="#2B344B",
+            font=("Inter", 12)
+        )
+        self.or_label.pack(pady=20)
+
+        # Admin Login Link
+        self.admin_btn = ctk.CTkButton(
+            self.main_frame,
+            text="🛡️ Admin Login",
+            fg_color="transparent",
+            hover_color="#161C30",
+            text_color="#555E70",
+            font=("Inter", 13),
+            height=40,
+            #command=self.admin_login_event
+        )
+        self.admin_btn.pack(pady=(10, 0))
+        
+
+if __name__ == "__main__":
+    app = LoginScreen()
+    app.mainloop()
