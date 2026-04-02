028bbca43774e2caaf6c9004d36d2d76bb3810a7
HarelValfish <harelvalfish@gmail.com>
2026-04-02 19:02:18 +0300

    making it work obviously

--- ui_windows/ui_admin_login.py
+++ ui_windows/ui_admin_login.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from ..models import * #! this doesnt work for some reason
+from models import * #! this doesnt work for some reason
 
 class AdminLoginScreen(ctk.CTk):
     def __init__(self):
@@ -120,16 +120,16 @@ class AdminLoginScreen(ctk.CTk):
         pass
 
 # if id and pin correct -> close window and open new dashboard window
-        if authenticate:
+        # if authenticate:
         
 
             # self.destroy()  #close windwow
             # admin_dashboard = admin_dashboard_screen() 
             # admin_dashboard.mainloop()
             
-def main():
-    app = AdminLoginScreen()
-    app.mainloop()
+    def main():
+        app = AdminLoginScreen()
+        app.mainloop()
     
             
 
