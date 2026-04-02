4442d601fe0848b31181974658edeab3acd46edc
toni7891 <toniv7891@gmail.com>
2026-04-03 01:56:07 +0300

    for pull

--- ui_windows/ui_login.py
+++ ui_windows/ui_login.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import ui_dashboard
 import ui_admin_login
 from PIL import Image
+import pywinstyles
 
 class LoginScreen(ctk.CTk):
     def __init__(self):
@@ -12,7 +13,7 @@ class LoginScreen(ctk.CTk):
         self.geometry("400x700") # screensize
         self.resizable(False, False) # Prevent the user from shrinking it manually
         self.update_idletasks() # Force the window to update its "inner math" before showing it
-        self.configure(fg_color="#0A0E27") # Background
+        #self.configure(fg_color="white", bg_color="white") # Background #0A0E27
         self.center_window() # Center the window on the screen
 
         # Main frame
@@ -27,102 +28,123 @@ class LoginScreen(ctk.CTk):
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         #main frame on top of bg image
-        self.main_frame = ctk.CTkFrame(self,width=400,height=400,corner_radius=15,fg_color="transparent") # fg_color="transparent"
-        self.main_frame.place(relx=0.5, rely=0.5, anchor="center", relwidth=0.8, relheight=0.7) # Padding on the sides of the main frame, so the content doesn't touch the edges of the window
+        # self.main_frame = ctk.CTkFrame(self, fg_color="transparent") # width=400,height=400,corner_radius=15,
+        # self.main_frame.place(relx=0.5, rely=0.5, anchor="center") # , relwidth=0.8, relheight=0.7
+         # Padding on the sides of the main frame, so the content doesn't touch the edges of the window
 
         # logo
         self.logo_label = ctk.CTkLabel(
-            self.main_frame, 
+            self, 
             text="A", # TODO REPLACE "A" LOGO WITH ACTUAL LOGO
             font=("Inter", 60, "bold"), 
-            text_color="#3B82F6")
-        self.logo_label.pack(pady=(60, 0))
-        
+            text_color="#3B82F6",
+            fg_color="transparent",
+            bg_color="transparent")
+        self.logo_label.place(relx=0.5, rely=0.3, anchor="center")
+        pywinstyles.set_opacity(self.logo_label, color="#000001")
         # bank name
         self.bank_name = ctk.CTkLabel(
-            self.main_frame, 
+            self, 
             text="ARGUMENT\nCAPITAL", 
             font=("Inter", 24, "bold"), 
-            text_color="white")
-        self.bank_name.pack(pady=(0, 0))
+            text_color="white",
+            fg_color="transparent",
+            bg_color="transparent")
+        self.bank_name.place(relx=0.5, rely=0.3, anchor="center")
+        pywinstyles.set_opacity(self.bank_name, color="#000001")
         
         # ATM APP as the assignment dictates
         self.app_name = ctk.CTkLabel(
-            self.main_frame, 
+            self, 
             text="----------------\nATM APP", 
             font=("Inter", 24, "bold"), 
-            text_color="#3B82F6")
+            text_color="#3B82F6",
+              fg_color="transparent",
+            bg_color="transparent")
         self.app_name.pack(pady=(0, 40))
+        pywinstyles.set_opacity(self.app_name, color="#000001")
 
         # account ID input field
         self.username_entry = ctk.CTkEntry(
-            self.main_frame,
+            self,
             placeholder_text="Account ID",
             height=55,
-            fg_color="#161C30",
             border_color="#90D5FF",
             text_color="white",
-            corner_radius=12
+            #corner_radius=12,
+            fg_color="transparent",
+            bg_color="transparent"
         )
         self.username_entry.pack(fill="x", pady=10)
         
         # password input field
         self.password_entry = ctk.CTkEntry(
-            self.main_frame,
+            self,
             placeholder_text="PIN",
             show="*", # hides input
             height=55,
-            fg_color="#161C30",
+            #fg_color="#161C30",
             border_color="#90d5ff",
             text_color="white",
-            corner_radius=12
+            #corner_radius=12,
+            fg_color="transparent",
+            bg_color="transparent"
         )
         self.password_entry.pack(fill="x", pady=10)
+        pywinstyles.set_opacity(self.password_entry, color="#000001")
 
         # password reset button
         self.forgot_btn = ctk.CTkButton(
-            self.main_frame,
+            self,
             text="Change PIN",
-            fg_color="transparent",
             hover=False,
             text_color="#555E70",
             font=("Inter", 12),
-            width=10
+            width=10,
+            fg_color="transparent",
+            bg_color="transparent"
             # TODO connect to change pin screen with command=
         )
         self.forgot_btn.pack(anchor="e", pady=(0, 20))
-
+        pywinstyles.set_opacity(self.forgot_btn, color="#000001")
         # login button
         self.login_btn = ctk.CTkButton(
-            self.main_frame,
+            self,
             text="Log In",
             height=55,
-            fg_color="#3B82F6",
+            #fg_color="#3B82F6",
             hover_color="#2563EB",
             font=("Inter", 16, "bold"),
-            corner_radius=12,
-            command= self.open_dashboard # TODO connect to authenticate function
+            #corner_radius=12,
+            command= self.open_dashboard,
+            fg_color="transparent",
+            bg_color="transparent" # TODO connect to authenticate function
         )
-        self.login_btn.pack(fill="x", pady=10)
+        self.login_btn.pack(fill="x", pady=10, padx=50)
+        pywinstyles.set_opacity(self.login_btn, color="#000001")
 
         # OR text seperating login or admin login
         self.or_label = ctk.CTkLabel(
-            self.main_frame, 
+            self, 
+            fg_color="transparent",
+            bg_color="transparent",
             text="────────  OR  ────────", 
             text_color="#2B344B", 
-            font=("Inter", 12))
+            font=("Inter", 12)
+        )
         self.or_label.pack(pady=10)
 
         #Admin Login Link
         self.admin_btn = ctk.CTkButton(
-            self.main_frame,
+            self,
             text="🛡️ Admin Login",
-            fg_color="transparent",
             hover_color="#161C30",
             text_color="#555E70",
             font=("Inter", 13),
             height=40,
-            command=self.open_admin_login # open admin login screen and close current login screen
+            command=self.open_admin_login, # open admin login screen and close current login screen
+            fg_color="transparent",
+            bg_color="transparent"
         )
         self.admin_btn.pack(pady=(0))
 
