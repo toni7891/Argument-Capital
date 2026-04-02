d93c62905df4048015bbdb8b218e8f31f34b6117
CaptainPeres <guyguyperes@gmail.com>
2026-04-02 22:49:18 +0300

    added more libaraies for use

--- ui_windows/ui_admin_user_table.py
+++ ui_windows/ui_admin_user_table.py
@@ -1,6 +1,18 @@
+
 import customtkinter as ctk
 from CTkTable import *
+import sys
+import os
 #?--> use the command if you dont have CTkTable > pip install CTkTable
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+import models
+import storage
+import json
 
 class Admin_user_table(ctk.CTk):
     def __init__(self):
